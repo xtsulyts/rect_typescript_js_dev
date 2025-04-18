8af177f75c66552f39c97d7246e6cf8d004cc8bb
xtsulyts <531xtsulyts@gmail.com>
2025-04-18 14:08:04 -0300

    Agragrego atributo imagen al objeto pruducto, defino estilo imagen-producto en ./src/componentes/estilos/Galeria.css

--- react_vite_js_22022/src/App.jsx
+++ react_vite_js_22022/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header';
 import Nav from './components/Nav';
 import TarjetaProyecto from './components/TajetaProyecto';
@@ -12,12 +12,12 @@ import './App.css'
 const App = () => {
 
   const productos = [
-    { id: 1, codigo: 1001, nombre: 'Producto A', imagen:  },
-    { id: 2, codigo: 1002, nombre: 'Producto B', cantidad: 3 },
-    { id: 3, codigo: 1003, nombre: 'Producto C', cantidad: 8 },
-    { id: 4, codigo: 1001, nombre: 'Producto A', cantidad: 5 },
-    { id: 5, codigo: 1002, nombre: 'Producto B', cantidad: 3 },
-    { id: 6, codigo: 1003, nombre: 'Producto C', cantidad: 8 },
+    { id: 1, codigo: 1001, nombre: 'Producto A', imagen: 'https://images.pexels.com/photos/267320/pexels-photo-267320.jpeg' },
+    { id: 2, codigo: 1002, nombre: 'Producto B', cantidad: 8,  imagen:'https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
+    { id: 3, codigo: 1003, nombre: 'Producto C', cantidad: 8, imagen: 'https://images.pexels.com/photos/1598508/pexels-photo-1598508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
+    { id: 4, codigo: 1001, nombre: 'Producto A', cantidad: 5, imagen:'https://images.pexels.com/photos/292998/pexels-photo-292998.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
+    { id: 5, codigo: 1002, nombre: 'Producto B', cantidad: 3, imagen: 'https://images.pexels.com/photos/1240892/pexels-photo-1240892.jpeg' },
+    { id: 6, codigo: 1003, nombre: 'Producto C', cantidad: 8, imagen: "https://images.pexels.com/photos/3602449/pexels-photo-3602449.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
   ]
 
   const proyecto = 
@@ -29,12 +29,12 @@ const App = () => {
 
   const handleProductClick = (productId) => {
     console.log('Producto seleccionado:', productId);
-    // Aquí podrías navegar a una página de detalle o mostrar un modal
+   
   }
   return (
     <>
     <Header titulo={"TRABAJO INTEGRADOR REACT"}/>
-    <div className="app">
+  
 
     <div>
       <Nav/>
@@ -44,6 +44,7 @@ const App = () => {
       <TarjetaProyecto
         nombreProyecto={proyecto.nombre}
         descripcionProyecto={proyecto.descripcion}
+
       />
       
     </div>
@@ -51,7 +52,7 @@ const App = () => {
         productos={productos} 
         onProductClick={handleProductClick}
       />
-    </div>
+   
    
     <Footer/>
     </>
--- react_vite_js_22022/src/components/Galeria.jsx
+++ react_vite_js_22022/src/components/Galeria.jsx
@@ -8,14 +8,24 @@ const Galeria = ({ productos, onProductClick }) => {
     <div className='galleryContainer'>  
       {productos.map((producto) => (
         <div 
+        
           key={producto.id} 
           className="productCard"
           onClick={() => onProductClick && onProductClick(producto.id)}
         >
+          
+          
           <div className="cardHeader">
             <span className="productCodigo">#{producto.codigo}</span>
           </div>
           <div className="cardBody">
+            {/* Agregamos la imagen aquí */}
+      {producto.imagen && (
+        <img className='producto-imagen'
+        src={producto.imagen}
+        alt={producto.nombre}
+ />
+      )}
             <h3 className="productNombre">{producto.nombre}</h3>
             <p className="productCantidad">Disponibles: {producto.cantidad}</p>
           </div>
